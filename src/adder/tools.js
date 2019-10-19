f3cb8fea29e3977500661307be07bb6d8e5264a7
Ikuta Masaya <masayanyanma1207@docomo.ne.jp>
2019-10-19 15:26:36 +0900

    パネル操作について追記

--- src/adder/tools.js
+++ src/adder/tools.js
@@ -10,36 +10,34 @@ function drag(className){
 function drop(){
   var x = 0;
   var y = 0;
-  $('.hobby-submitbox').droppable({
-    active:function(e,ui){
-      x = 0;
-      y = 0;
+  $("html").droppable({
+    deactive: function(e,ui){
+      $(ui.draggable)
+        .css("top",0)
+        .css("left",0)
+        .css("color","blue");
     },
+    drop: function(e,ui) {
+      $(ui.draggable)
+        .css("top",0)
+        .css("left",0)
+        .css("color","blue");
+    }
+  });
+  $('.hobby-submitbox').droppable({
     over: function(e,ui) {
-      $(this)
-        .find("h3")
-        .css("color","white");
       $(ui.draggable)
         .css("color","blue");
     },
     out: function(e,ui) {
-      $(this)
-        .find("h3")
-        .css("color","black");
       $(ui.draggable)
         .css("color","blue")
     },
     deactive: function(e,ui){
       $(ui.draggable)
-        .css("color","black")
-        .css("top",y)
-        .css("left",x);
+        .css("color","blue");
     },
     drop: function(e,ui) {
-      $(ui.draggable)
-        .css("color","black")
-        .css("top",y)
-        .css("left",x);
     }
   });
 }
